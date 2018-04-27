e8beb1808c7574f9a67cfb8cacce6f568a097bef
Maximilian Schuler <mschuler@itemis.com>
2018-04-27 15:07:57 +0200

    Add ruleset

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -6,5 +6,19 @@ export enum Cell {
 export type Neighborhood = Cell[]
 
 export function update(neighborhood: Neighborhood): Cell {
+    const centerCell = neighborhood[4]
+    let alive = neighborhood.filter(cell => cell === Cell.Alive).length
+    if (centerCell === Cell.Alive) {
+        alive -= 1
+        if (alive > 3) {
+            return Cell.Dead
+        } else if (alive > 1) {
+            return Cell.Alive
+        } else {
+            return Cell.Dead
+        }
+    } else if (alive === 3) {
+        return Cell.Alive
+    }
     return neighborhood[4]
 }
